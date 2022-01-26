0e7f241c6cc0ad64ceca0766550a7837c8fcb67d
Edvin Mulabdic <edvin.mulabdic@corvus-info.com>
2022-01-26 16:04:11 +0100

    pass hash

--- app/models/AppUser.java
+++ app/models/AppUser.java
@@ -61,13 +61,7 @@ public class AppUser extends Model {
 
         AppUser user = finder.where().eq("email", email).findUnique();
 
-//        if (user != null && BCrypt.checkpw(password, user.password)) {
-//            return user;
-//        } else {
-//            return null;
-//        }
-
-        if (user != null && password.equals(user.password)) {
+        if (user != null && BCrypt.checkpw(password, user.password)) {
             return user;
         } else {
             return null;
